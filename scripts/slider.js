98d706c6acf07bb28c6b21d083e387d1438104fb
DaniilPolskov <113332182+DaniilPolskov@users.noreply.github.com>
2024-05-06 15:21:23 +0300

    new slider and font-text
    added pause for slider and updated text

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,8 +1,10 @@
 let slideIndex = 0;
 const intervalTime = 3500;
+let slides;
+let intervalId; // Переменная для хранения идентификатора интервала
 
 function showSlides() {
-  const slides = document.querySelectorAll('.slides img');
+  slides = document.querySelectorAll('.slides img'); 
   slideIndex++;
   if (slideIndex >= slides.length) {
     slideIndex = 0;
@@ -11,4 +13,28 @@ function showSlides() {
   document.querySelector('.slides').style.transform = `translateX(${-slideIndex * slideWidth}px)`;
 }
 
-setInterval(showSlides, intervalTime);
+intervalId = setInterval(showSlides, intervalTime); 
+
+document.querySelector('.prev').addEventListener('click', prevSlide);
+document.querySelector('.next').addEventListener('click', nextSlide);
+
+function prevSlide() {
+  clearInterval(intervalId); 
+  slideIndex--;
+  if (slideIndex < 0) {
+    slideIndex = slides.length - 1;
+  }
+  const slideWidth = slides[0].clientWidth;
+  document.querySelector('.slides').style.transform = `translateX(${-slideIndex * slideWidth}px)`;
+}
+
+function nextSlide() {
+  clearInterval(intervalId); 
+  slideIndex++;
+  if (slideIndex >= slides.length) {
+    slideIndex = 0;
+  }
+  const slideWidth = slides[0].clientWidth;
+  document.querySelector('.slides').style.transform = `translateX(${-slideIndex * slideWidth}px)`;
+}
+
