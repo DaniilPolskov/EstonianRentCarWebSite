340125424cf5b85f1400149ede7bd8f7b22ff48f
DaniilPolskov <113332182+DaniilPolskov@users.noreply.github.com>
2024-05-07 20:54:02 +0300

    New slider, fixed footer, slide text
    The script for slider is finalized, new buttons for slider are created and added, text to pictures in slider is added, error with footer and slider is eliminated.

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,10 +1,10 @@
 let slideIndex = 0;
-const intervalTime = 3500;
+const intervalTime = 2500;
 let slides;
-let intervalId; // Переменная для хранения идентификатора интервала
+let intervalId;
 
 function showSlides() {
-  slides = document.querySelectorAll('.slides img'); 
+  slides = document.querySelectorAll('.slides img');
   slideIndex++;
   if (slideIndex >= slides.length) {
     slideIndex = 0;
@@ -13,28 +13,24 @@ function showSlides() {
   document.querySelector('.slides').style.transform = `translateX(${-slideIndex * slideWidth}px)`;
 }
 
-intervalId = setInterval(showSlides, intervalTime); 
+intervalId = setInterval(showSlides, intervalTime);
 
-document.querySelector('.prev').addEventListener('click', prevSlide);
-document.querySelector('.next').addEventListener('click', nextSlide);
-
-function prevSlide() {
-  clearInterval(intervalId); 
+document.querySelector('.prev').addEventListener('click', () => {
+  clearInterval(intervalId);
   slideIndex--;
   if (slideIndex < 0) {
     slideIndex = slides.length - 1;
   }
   const slideWidth = slides[0].clientWidth;
   document.querySelector('.slides').style.transform = `translateX(${-slideIndex * slideWidth}px)`;
-}
+});
 
-function nextSlide() {
-  clearInterval(intervalId); 
+document.querySelector('.next').addEventListener('click', () => {
+  clearInterval(intervalId);
   slideIndex++;
   if (slideIndex >= slides.length) {
     slideIndex = 0;
   }
   const slideWidth = slides[0].clientWidth;
   document.querySelector('.slides').style.transform = `translateX(${-slideIndex * slideWidth}px)`;
-}
-
+});
