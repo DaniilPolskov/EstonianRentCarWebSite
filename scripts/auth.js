5503010f4133b5777dcf840ec82aff8669f7f59a
DaniilPolskov <113332182+DaniilPolskov@users.noreply.github.com>
2024-05-27 15:19:28 +0300

    first rent page car (one)
    release characterization table for car and style for images

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,22 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var singupLink = document.getElementById('singup');
-  
-    var modal = document.getElementById('modal');
-  
-    var closeBtn = document.getElementsByClassName('close')[0];
-  
-    singupLink.addEventListener('click', function() {
+  var singupLink = document.getElementById('singup');
+  var modal = document.getElementById('modal');
+  var closeBtn = document.getElementsByClassName('close')[0];
+  var body = document.querySelector('body');
+
+  singupLink.addEventListener('click', function() {
       modal.style.display = 'block';
-    });
-  
-    closeBtn.addEventListener('click', function() {
-      modal.style.display = 'none';
-    });
-  
-    window.addEventListener('click', function(event) {
-      if (event.target == modal) {
-        modal.style.display = 'none';
-      }
-    });
+      body.classList.add('modal-open');
   });
-  
+
+  closeBtn.addEventListener('click', function() {
+      modal.style.display = 'none';
+      body.classList.remove('modal-open');
+  });
+
+  window.addEventListener('click', function(event) {
+      if (event.target == modal) {
+          modal.style.display = 'none';
+          body.classList.remove('modal-open');
+      }
+  });
+});
